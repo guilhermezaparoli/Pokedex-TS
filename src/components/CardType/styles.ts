3bb9a0481b7e2b498f55462cd381283a1ae6c88d
Guilherme <guilhermezapas@gmail.com>
2023-09-11 07:49:11 -0300

    Search by types change colors

--- src/components/CardType/styles.ts
+++ src/components/CardType/styles.ts
@@ -27,7 +27,8 @@ export interface StatusProps {
 }
 
 export const StyledContainer = styled.button<StatusProps>`
-background-color: ${props => props.isSelected ? props.theme[STATUS_COLORS[props.statusColor]]  : `#060F39`};
+/* background-color: ${props => props.isSelected ? props.theme[STATUS_COLORS[props.statusColor]]  : `#060F39`}; */
+background-color: ${props => props.theme[STATUS_COLORS[props.statusColor]]};
 
 display: flex;
 align-items: center;
@@ -39,16 +40,17 @@ border: 1px solid transparent;
 transition: 0.3s;
 
 p {
-  color: ${props => props.isSelected ? props.theme.white : props.theme[STATUS_COLORS[props.statusColor]]} ;
+  /* color: ${props => props.isSelected ? props.theme.white : props.theme[STATUS_COLORS[props.statusColor]]} ; */
+  color: ${props => props.theme.white} ;
   font-size: 1rem;
   line-height: 1.5;
 
   text-transform: capitalize;
 }
 
-svg path {
+/* svg path {
   fill: ${props => props.isSelected ? props.theme.white : props.theme[STATUS_COLORS[props.statusColor]]} !important;
-}
+} */
 
 &:hover {
   border: 1px solid  ${props => props.theme[STATUS_COLORS[props.statusColor]]}
