ee0bd0e3e731471ab05871145eb4b4854845138b
Guilherme <guilhermezapas@gmail.com>
2023-09-11 07:50:27 -0300

    Search by types change colors

--- src/components/CardType/styles.ts
+++ src/components/CardType/styles.ts
@@ -27,7 +27,8 @@ export interface StatusProps {
 }
 
 export const StyledContainer = styled.button<StatusProps>`
-background-color: ${props => props.isSelected ? props.theme[STATUS_COLORS[props.statusColor]]  : `#060F39`};
+/* background-color: ${props => props.isSelected ? props.theme[STATUS_COLORS[props.statusColor]]  : `#060F39`}; */
+background-color: ${props => props.theme[STATUS_COLORS[props.statusColor]]};
 
 display: flex;
 align-items: center;
@@ -35,24 +36,24 @@ gap: 8px;
 
 padding: 4px 8px;
 border-radius: 8px;
-border: 1px solid transparent;
+/* border: 1px solid transparent; */
 transition: 0.3s;
 
 p {
-  color: ${props => props.isSelected ? props.theme.white : props.theme[STATUS_COLORS[props.statusColor]]} ;
+  color: ${props => props.theme.white} ;
   font-size: 1rem;
   line-height: 1.5;
 
   text-transform: capitalize;
 }
 
-svg path {
+/* svg path {
   fill: ${props => props.isSelected ? props.theme.white : props.theme[STATUS_COLORS[props.statusColor]]} !important;
-}
+} */
 
-&:hover {
+/* &:hover {
   border: 1px solid  ${props => props.theme[STATUS_COLORS[props.statusColor]]}
 
-}
+} */
 
 `
